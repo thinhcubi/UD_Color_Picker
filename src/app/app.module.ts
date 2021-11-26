fd9b7d07d970aa7e89e11ed7fb2e83c2eed4766a
thinhcubi <thinhcubi27@gmail.com>
2021-11-26 09:47:21 +0700

    demo

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
+import {FormsModule} from "@angular/forms";
+import { PetComponent } from './pet/pet.component';
+import { ProductComponent } from './product/product.component';
+import { ColorComponent } from './color/color.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FontSizeEditorComponent,
+    PetComponent,
+    ProductComponent,
+    ColorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
